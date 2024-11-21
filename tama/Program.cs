11a44abdb7cbf59d364d730fd60202252c444850
Sadik Cay <sadik.cay@elev.ga.ntig.se>
2024-11-21 11:14:11 +0100

    .kjughllkug

---
+++ tama/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+int correct = 7;
+int number = 0;
+
+while (number != correct)
+{
+
+    bool kundeKonvertera = false;
+    while (kundeKonvertera == false)
+    {
+        Console.WriteLine("guess a number");
+        string svar = Console.ReadLine();
+        kundeKonvertera = int.TryParse(svar, out number);
+
+        if (kundeKonvertera == false)
+        {
+            Console.WriteLine("try again");
+        }
+    }
+
+    if (number == correct)
+    {
+        Console.WriteLine("congrats");
+    }
+    else if (number > correct)
+    {
+        Console.WriteLine("too high");
+    }
+    else if (number < correct)
+    {
+        Console.WriteLine("too low");
+    }
+}
+
+Console.ReadLine();
